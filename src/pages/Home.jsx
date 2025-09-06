98e40ec1ac917ceca0ed270aaba4d02634a92859
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2025-09-06 13:27:39 +0100

    feat: add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { StarBackground } from "@/components/StarBackground"
 import { ThemeToggle } from "@/components/ThemeToggle"
 import { Navbar } from "@/components/Navbar"
+import { HeroSection } from "@/components/HeroSection"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -15,6 +16,10 @@ export const Home = () => {
         <Navbar />
 
         {/* Main content */}
+        <main>
+            <HeroSection />
+
+        </main>
 
         {/* Footer */}
 
